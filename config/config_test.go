56bac52d6aa141ed3cf170b138fd03cccc6f27a2
aibotsoft <aibotsoft@gmail.com>
2020-03-25 19:16:24 +0200

    config

---
+++ config/config_test.go
@@ -0,0 +1,22 @@
+package config_test
+
+import (
+	"github.com/aibotsoft/micro/config"
+	"github.com/stretchr/testify/assert"
+	"os"
+	"testing"
+)
+
+func TestLoadEnv(t *testing.T) {
+	if !config.IsDev() {
+		return
+	}
+	err := config.LoadEnv()
+	assert.NoError(t, err)
+	assert.Equal(t, "true", os.Getenv("TEST_LOAD_ENV"))
+}
+
+func TestNew(t *testing.T) {
+	cfg := config.New()
+	assert.Equal(t, true, cfg.Service.TestLoadEnv)
+}
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.14
 
 require (
 	github.com/dgraph-io/ristretto v0.0.2
-	github.com/stretchr/testify v1.4.0
-	go.uber.org/zap v1.14.1
+	github.com/kelseyhightower/envconfig v1.4.0
+	github.com/stretchr/testify v1.5.1
+	github.com/subosito/gotenv v1.2.0
 )

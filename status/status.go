f02ae183eee833b8f1a770a0c2069b9afad8bf82
aibotsoft <aibotsoft@gmail.com>
2020-05-06 14:33:09 +0300

    coding

---
+++ status/status.go
@@ -0,0 +1,10 @@
+package status
+
+const (
+	StatusOk          = "Ok"
+	StatusNotFound    = "NotFound"
+	StatusError       = "Error"
+	StatusNotAccepted = "NotAccepted"
+	StatusDisabled    = "Disabled"
+	RegimeSurebet     = "Surebet"
+)

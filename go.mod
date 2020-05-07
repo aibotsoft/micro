a67abb55e19eaf6843aab9e364c8f3e0b41d50f9
aibotsoft <aibotsoft@gmail.com>
2020-05-07 18:45:47 +0300

    feat: resty_util.go

--- go.mod
+++ go.mod
@@ -5,8 +5,10 @@ go 1.14
 require (
 	github.com/denisenkom/go-mssqldb v0.0.0-20200206145737-bbfc9a55622e
 	github.com/dgraph-io/ristretto v0.0.2
+	github.com/go-resty/resty/v2 v2.2.0
 	github.com/golang-migrate/migrate/v4 v4.10.0
 	github.com/jackc/pgx/v4 v4.5.0
+	github.com/jmoiron/sqlx v1.2.0
 	github.com/pkg/errors v0.9.1
 	github.com/shopspring/decimal v0.0.0-20180709203117-cd690d0c9e24
 	github.com/stretchr/testify v1.5.1
---
+++ resty_util/resty_util.go
@@ -0,0 +1,20 @@
+package resty_util
+
+import (
+	"github.com/go-resty/resty/v2"
+	"go.uber.org/zap"
+)
+
+func RestyTrace(log *zap.SugaredLogger, resp *resty.Response) {
+	ti := resp.Request.TraceInfo()
+	log.Infow("Request Trace Info", "trace", ti)
+	//fmt.Println("DNSLookup    :", ti.DNSLookup)
+	//fmt.Println("ConnTime     :", ti.ConnTime)
+	//fmt.Println("TLSHandshake :", ti.TLSHandshake)
+	//fmt.Println("ServerTime   :", ti.ServerTime)
+	//fmt.Println("ResponseTime :", ti.ResponseTime)
+	//fmt.Println("TotalTime    :", ti.TotalTime)
+	//fmt.Println("IsConnReused :", ti.IsConnReused)
+	//fmt.Println("IsConnWasIdle:", ti.IsConnWasIdle)
+	//fmt.Println("ConnIdleTime :", ti.ConnIdleTime)
+}

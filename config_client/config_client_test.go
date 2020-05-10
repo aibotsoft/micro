010b0feb78006c10e19d393535d6bbf9bfefe0cd
aibotsoft <aibotsoft@gmail.com>
2020-05-10 16:22:18 +0300

    feat: add currency

--- config_client/config_client_test.go
+++ config_client/config_client_test.go
@@ -17,18 +17,12 @@ func TestMain(m *testing.M) {
 	m.Run()
 	c.Close()
 }
-
-func TestNewConfigClient(t *testing.T) {
-	assert.NotEmpty(t, c)
-}
-
 func TestConfClient_GetAccount(t *testing.T) {
 	got, err := c.GetAccount(context.Background(), "hello")
 	if assert.NoError(t, err) {
 		assert.NotEmpty(t, got)
 	}
 }
-
 func TestConfClient_GetGrpcAddr(t *testing.T) {
 	c.cfg.Service.Name = "config-service"
 	got, err := c.GetGrpcAddr(context.Background())
